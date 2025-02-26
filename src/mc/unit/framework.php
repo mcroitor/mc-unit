a986c279904092c8d4c3f1643c6089213e0efc1f
Mihail Croitor <mihail.croitor@usm.md>
2025-02-26 16:59:47 +0200

    add some new functions

--- src/mc/unit/framework.php
+++ src/mc/unit/framework.php
@@ -2,6 +2,11 @@
 
 namespace mc\unit;
 
+use \mc\unit\Unit;
+
+/**
+ * The unit test framework
+ */
 class Framework {
     /**
      * List of tests
@@ -14,15 +19,62 @@ class Framework {
      */
     private $passed = 0;
 
+    /**
+     * Constructor
+     */
+    public function __construct() {
+        $this->Reset();
+        $this->scan_tests();
+    }
+
+    private static function method_has_attribute($methodReflection, $attributeName) {
+        /** @var \ReflectionAttribute $attributes */
+        $attributes = $methodReflection->getAttributes();
+        foreach ($attributes as $attribute) {
+            if ($attribute->getName() == $attributeName) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * Scan tests, marked by @test annotation
+     * @return void
+     */
+    private function scan_tests(): void {
+        $functions = get_defined_functions();
+        foreach($functions['user'] as $function){
+            $reflection = new \ReflectionFunction($function);
+            if(self::method_has_attribute($reflection, Unit::class)){
+                $this->AddTest($function);
+            }
+        }
+    }
+
+    /**
+     * Reset test results
+     * @return void
+     */
     public function Reset() {
         $this->passed = 0;
         Assert::Reset();
     }
 
+    /**
+     * Add a test
+     * @param string $testName
+     * @return void
+     */
     public function AddTest(string $testName): void {
         $this->tests[$testName] = new Test($testName);
     }
 
+    /**
+     * Run a test
+     * @param string $testName
+     * @return void
+     */
     public function RunTest($testName): void {
         if(empty($this->tests[$testName])){
             echo "[WARN] test {$testName} was not found." . PHP_EOL;
@@ -34,24 +86,48 @@ class Framework {
         }
     }
 
+    /**
+     * Run all tests
+     * @return void
+     */
     public function Run(): void {
+        echo "[INFO] Running tests..." . PHP_EOL;
+        echo "[INFO] --------------------------------" . PHP_EOL;
         foreach($this->tests as $testName => $test){
             $this->RunTest($testName);
+            echo "[INFO] --------------------------------" . PHP_EOL;
         }
+        echo "[INFO] Tests completed." . PHP_EOL;
     }
 
+    /**
+     * Get number of tests
+     * @return int
+     */
     public function TotalTests(): int {
         return count($this->tests);
     }
 
+    /**
+     * Get number of tests passed
+     * @return int
+     */
     public function TotalPassed(): int {
         return $this->passed;
     }
 
+    /**
+     * Get number of tests failed
+     * @return int
+     */
     public function TotalFailed(): int {
         return $this->TotalTests() - $this->TotalPassed();
     }
 
+    /**
+     * Print test results
+     * @return void
+     */
     public function PrintInfo(): void {
         echo "=============== TESTS ================" . PHP_EOL;
         echo "total: {$this->TotalTests()}, passed: {$this->TotalPassed()}, failed: {$this->TotalFailed()}" . PHP_EOL;
---
+++ src/mc/unit/unit.php
@@ -0,0 +1,5 @@
+<?php
+
+namespace mc\unit;
+
+class Unit{}
